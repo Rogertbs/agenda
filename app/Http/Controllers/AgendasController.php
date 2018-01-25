ba8a9bab35bd679efd78f938e7fffa3dbdcd3138
Rogério <rogerio.santos@direitodeouvir.com.br>
2018-01-25 12:40:12 -0200

    controller agenda

--- app/Http/Controllers/AgendasController.php
+++ app/Http/Controllers/AgendasController.php
@@ -11,4 +11,63 @@ class AgendasController extends Controller
     public function __construct() {
        $this->middleware('jwt.auth');
    }
+
+   public function index()
+   {
+       $agendas = Agendas::all();
+       return response()->json($agendas);
+   }
+
+   public function show($id)
+   {
+       $agenda = Agendas::find($id);
+
+       if(!$agenda){
+           return response()->json([
+               'message' => 'Record not found',
+           ], 404);
+       }
+
+       return response()->json($agenda);
+   }
+
+   public function store(Request $req)
+   {
+       $agenda = new Agendas();
+       $agenda->fill($req->all());
+       $agenda->save();
+
+       return response()->json($agenda, 201);
+   }
+
+   public function update(Request $req, $id)
+   {
+       $agenda = Agendas::find($id);
+
+       if(!$medico){
+           return response()->json([
+               'message' => 'Record not found',
+           ],404);
+       }
+
+       $agenda->fill($req->all());
+       $agenda->save();
+
+       return response()->json($agenda, 200);
+   }
+
+   public function destroy($id)
+   {
+       $agenda = Agendas::find($id);
+
+       if(!$agenda){
+           return response()->json([
+               'message' => 'Record not found',
+           ], 400);
+       }
+
+       $agenda->delete();
+
+       return response()->json(['message', 'Agenda deletada com sucesso!'], 202);
+   }
 }
